57cd3a220b1218cf6ed629e6e378eda941839ab2
luizfernandofolchini <147443269+luizfernandofolchini@users.noreply.github.com>
2025-08-19 14:43:20 -0300

    19/08

--- main.js
+++ main.js
@@ -22,3 +22,21 @@ function aumentaTamanho(){
     numeroSenha.textContent=tamanhoSenha;
     geraSenha();
 }
+
+const campoSenha = document.querySelector('#campo-senha');
+
+const letraMaiuscula = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ'
+const letraMinuscula = 'abcdefghijklmnopqrstuvwxyz'
+const numeros = '0123456789';
+const simbolo = '!@%$*¨'
+geraSenha();
+
+function geraSenha() {
+    let senha = '';
+    for (let i=0; 1<tamanhoSenha; i++){
+        let numeroAleatorio = Math.random()*letrasMaiusculas.lenth;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio];
+    }
+    campoSenha.value = senha;
+}
