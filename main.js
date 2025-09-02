78e00b8d616b1b1d0a46cfb57885a6a589e602c1
luizfernandofolchini <147443269+luizfernandofolchini@users.noreply.github.com>
2025-09-02 13:50:59 -0300

    02/09/25

--- main.js
+++ main.js
@@ -67,10 +67,13 @@ function geraSenha() {
     }
     campoSenha.value = senha;
     classificaSenha();
+}
+
+function classificaSenha (){
     forcaSenha.classList.remove('fraca', 'media', 'forte')
     if (tamanhoSenha > 11) {
         forcaSenha.classList.add('forte');
-    } else if (tamanhoSenha> 5 && tamanhoSenha < 12) {
+    } else if (tamanhoSenha > 5 && tamanhoSenha < 12) {
         forcaSenha.classList.add('media');
     } else if (tamanhoSenha <= 5) {
         forcaSenha.classList.add('fraca');
