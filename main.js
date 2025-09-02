801b0d2e914f48e7bf6974ef4aefd411f7071260
luizfernandofolchini <147443269+luizfernandofolchini@users.noreply.github.com>
2025-09-02 14:28:53 -0300

    02/09/25

--- main.js
+++ main.js
@@ -82,7 +82,7 @@ function classificaSenha(tamanhoAlfabeto) {
         forcaSenha.classList.add('fraca');
     }
     const ValorEntropia = document.querySelector('.entropia');
-    ValorEntropia.textContent = Math.floor(2**entropia/(100e6*60*60*24));
+    ValorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
 }
 
 
